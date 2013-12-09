9fe603edf102623b40b38c826df37bff9860105e
Soeren Horn <soeren-horn@gmx.net>
2013-12-09 13:18:34 +0100

    problem

--- html_jquery/details.php
+++ html_jquery/details.php
@@ -7,31 +7,43 @@
 </head>
 <body>
 <script>
-function parse(template, data) {
+      function parse(template, data) {
   return template.replace(/\{([\w_]+)\}/g, function(tag, key) {
   return data[key] || tag;
   });
 }
+  document.addEventListener('DOMContentLoaded', function(){
 
-var template = '<h1>{title}</h1>'+
-  '<p> Datum: {date}</p>'+
-  '<p> Beschreibung: {descr}</p>'+
-  '<p> Sprachen: {lang}</p>';
 
+var template = $('#content').html();
+debugger
+console.log(template);
 var id = '<?php echo $_REQUEST["id"]?>';
 var json = 'details' + id + '.json';
 $.getJSON(json, function(data) {
   var html = '';
   html += parse(template, data);
-
-  $( '<p/>', {
-  html: html
-  }).appendTo('#content');
+$('#content').html(html);
 });
+
+  });
 
 </script>
 
-<p id='content'></p>
+<p id="content">
+  <h1>{title}</h1>
+  <p> Datum: {date}</p>
+  <p> Beschreibung: {descr}</p>
+  <p> Sprachen: {lang}</p>
+</p>
+
+<p id='buttons'>
+<button name='Edit' type='button'
+  value='Editieren' onclick='alert("Editieren");'>EDIT</button>
+<button name='Delete' type='button'
+  value ='Loeschen' onclick='alert("Löschen");'>DELETE</button>
+</p>
 <a href='index.php'>Zurück zur Liste</a>
+
 </body>
 </html>
