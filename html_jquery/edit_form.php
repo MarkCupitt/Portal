4500a8fce5d4d8fd535dc71a56e9a2e9a8fdcc55
Soeren Horn <soeren-horn@gmx.net>
2013-12-16 11:57:27 +0100

    ajax extension for post

--- html_jquery/edit_form.php
+++ html_jquery/edit_form.php
@@ -18,9 +18,20 @@ var id = '<?php echo $_REQUEST["id"]?>';
 
 $(function(){
   $('#btn_save').click(function() {
-    $('.input').each(function(i, field) {
-      _data[field.name] = field.value;
+  $('.input').each(function(i, field) {
+    _data[field.name] = field.value;
     });
+
+  $.ajax({
+  type: "POST",
+  url: 'server/?details/id/'+ id,
+  data: _data,
+  dataType: 'json',
+  success: function(){
+    alert('Works!');
+  }
+  });
+  console.log(JSON.stringify(_data));
   });
 
   $('#btn_cancel').click(function() {
@@ -41,7 +52,7 @@ $(function(){
 
 <div id="content">
   <h1>Form for data editing</h1>
-  <form>
+  <form id="editForm">
   <p>Titel:<br><input name="title" class="input" type="text" size="30" maxlength="30"></p>
   <p>Datum:<br><input name="date" class="input" type="text" size="30" maxlength="30"></p>
   <p>Beschreibung:<br><textarea name="descr" class="input" cols="50" rows="10"></textarea></p>
