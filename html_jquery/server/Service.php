ac4185bce08f75e34d2aa53d1d62ed4c932ee7e2
Jan Marsch <jama@keks.com>
2014-01-13 14:26:34 +0100

    import ns fix

--- html_jquery/server/Service.php
+++ html_jquery/server/Service.php
@@ -93,15 +93,14 @@ WHERE
     $str = $HTTP->sendRequest($filter["url"]);
 
     $xml = simplexml_load_string($str);
+    $xml->registerXPathNamespace("wms", "http://www.opengis.net/wms");
 
-    $ns = $xml->getDocNamespaces();
+//    $ns = $xml->getDocNamespaces();
 
     // http://cmgds.marine.usgs.gov/geoserver/bathy/ows?SERVICE=WMS&SERVICE=WMS&REQUEST=GetCapabilities
     // => http://localhost:8000/osm/Portal/html_jquery/server/index.php?import&url=http%3A//cmgds.marine.usgs.gov/geoserver/bathy/ows%3FSERVICE%3DWMS%26SERVICE%3DWMS%26REQUEST%3DGetCapabilities
 
-//    $xml->registerXPathNamespace("gml", "http://www.opengis.net/gml");
-//    $bbox = $xml->xpath("gml:boundedBy/gml:Envelope/*");
-
+$res = $xml->xpath("wms:Service/wms:Title");
     return $res;
   }
 
