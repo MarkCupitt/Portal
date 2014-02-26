e5e2824d220d637c336bce536d83f0ea4c6861ea
Soeren Horn <soeren-horn@gmx.net>
2014-02-26 16:11:00 +0100

    wfs parser

--- server/XmlStream.js
+++ server/XmlStream.js
@@ -2,10 +2,21 @@ var util   = require('util');
 var events = require('events');
 var http = require('http');
 var fs = require('fs');
+var proj = require("proj4");
+var config = require('./config.js');
 
 function write(str, file) {
 	fs.writeFileSync(file, str, 'utf8');
 }
+
+//var templateGeoJson = '{"type":"FeatureCollection",' +
+//  '"features":[{"type":"Feature",' +
+//  '"properties":{},' +
+//  '"geometry":{"type":"{geometryType}",' +
+//  '"coordinates":[]' +
+//  '}}]}\n';
+
+fs.writeFile('./TESTgeojson.xml', '{"type":"FeatureCollection","features":[\n');
 
 var url = "http://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_hausumringe?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=fis:re_hausumringe&SRSNAME=EPSG:3068";
 
@@ -35,7 +46,7 @@ var XmlStream = exports.XmlStream = function(url) {
         str = str.substring(m[0].length);
       }
     }.bind(this))
-    .on('close', function () {
+    .on('end', function () {
       this.emit('end');
     }.bind(this))
   }.bind(this));
@@ -47,62 +58,53 @@ var proto = XmlStream.prototype;
 var stack = [];
 var feature = {};
 var json_str = '';
+var geoJson_str = '';
+var geojson = {type:'Feature', properties:{}, geometry:{coordinates:[[]]}};
+var coord_x = [],coord_y = [];
+var flag = false;
 
 var xml = new XmlStream(url)
   .on('tag-open', function(args) {
-//    console.log(args);
     stack.push(args.name);
 
     if (args.name === 'gml:featureMember'){
-
-//      console.log(feature);
-      feature = {};
+     if (geojson.geometry.type !== undefined && geojson.geometry.coordinates[0].length){
+        fs.appendFile('./TESTgeojson.xml', JSON.stringify(geojson) + ',\n');
+        geojson = {type:'Feature', properties:{}, geometry:{coordinates:[[]]}};
+      }
     }
     if (stack[stack.length-2] === 'gml:geometryMember'){
-      // Bisher vorandene feature in ein array schreiben
-      feature.geometry = args.name;
-        fs.appendFile('./hallo.xml',feature.geometry + '\n', function (err) {
-        if (err) throw err;
-        console.log('The "data to append" was appended to file!');
-        });
+      geojson.geometry.type = args.name.split(':')[1];
     }
   })
   .on('tag-close', function(args) {
 //    console.log(args);
-    stack.pop();
+   if (args.name === 'wfs:FeatureCollection'){
+     console.log('HALLO');
+     fs.appendFile('./TESTgeojson.xml', 'ENDE');
+   }
+   stack.pop();
   })
   .on('text', function(args) {
-//    console.log(args);
-    if (stack.join('/') === 'wfs:FeatureCollection/gml:boundedBy/gml:Box/gml:coordinates'){
-//      console.log(args.text);
-      feature.bbox_coord = args.text;
-      json_str = 'BBox Coordinates ' + '\n' + feature.bbox_coord;
-        fs.writeFile('./hallo.xml', json_str, function (err) {
-          if (err) throw err;
-          console.log('It\'s saved!');
-        });
-    }
     if (stack[stack.length-1] === 'gml:coordinates'){
-      // Bisher vorandene feature in ein array schreiben
-      feature.coordinates = args.text;
-       fs.appendFile('./hallo.xml',feature.coordinates + '\n', function (err) {
-        if (err) throw err;
-        console.log('The "data to append" was appended to file!');
-        });
+      if (stack[stack.length-2] !== 'gml:Box'){
+        var coord_split = args.text.split(' ');
+
+        for (var i=0; i<coord_split.length; i++){
+          coords = coord_split[i].split(',');
+//          coord_x = parseFloat(coords[0]);
+//          coord_y = parseFloat(coords[1]);
+
+          coord_x = proj(config.proj['EPSG:25833'], config.proj['EPSG:4326'], [parseFloat(coords[0]),parseFloat(coords[1])])[0];
+          coord_y = proj(config.proj['EPSG:25833'], config.proj['EPSG:4326'], [parseFloat(coords[0]),parseFloat(coords[1])])[1];
+
+          geojson.geometry.coordinates[0].push([coord_x, coord_y]);
+        }
+      }
+    } else {
+      geojson.properties[stack[stack.length-1]] = args.text;
     }
+  })
+  .on('end', function(){
+    fs.appendFile('./TESTgeojson.xml', ']}');
   });
-
-
-
-//fs.writeFile('./hallo.xml', json_str, function (err) {
-//  if (err) throw err;
-//  console.log('It\'s saved!');
-//});
-//write(feature.geometry, './hallo.xml');
-
-// ATTRIB
-//var str = ' href="213" src="test"', m;
-//while (m = str.match(/\s*([a-z_:-]+)="([^"]*)"/)) {
-//  console.log(m);
-//  str = str.substring(m[0].length)
-//};
